52f61660debb9c80e321cf7dcc5a667410ac353a
Morin Sultan <sultanmorin2001@gmail.com>
2024-05-28 00:03:59 +0100

    init

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const itemCount = document.getElementById("itemCount");
-  const increaseButton = document.getElementById("increase");
-  const decreaseButton = document.getElementById("decrease");
+const mainImg = document.querySelectorAll(".product-image img");
+const thumblineImg = document.querySelectorAll(
+  ".product-thumbnails .img-container"
+);
 
-  let count = 0;
-
-  increaseButton.addEventListener("click", () => {
-    count++;
-    updateCount();
-  });
-
-  decreaseButton.addEventListener("click", () => {
-    if (count > 0) {
-      count--;
-      updateCount();
+thumblineImg.forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    const currentImage = e.currentTarget.classList;
+    console.log(currentImage);
+    if (!currentImage.contains("active")) {
+      currentImage.add("active");
+    } else {
+      currentImage.remove("active");
     }
   });
-
-  function updateCount() {
-    itemCount.textContent = count;
-  }
 });
+
+console.log(mainImg);
+// document.addEventListener("DOMContentLoaded", () => {
+//   const itemCount = document.getElementById("itemCount");
+//   const increaseButton = document.getElementById("increase");
+//   const decreaseButton = document.getElementById("decrease");
+
+//   let count = 0;
+
+//   increaseButton.addEventListener("click", () => {
+//     count++;
+//     updateCount();
+//   });
+
+//   decreaseButton.addEventListener("click", () => {
+//     if (count > 0) {
+//       count--;
+//       updateCount();
+//     }
+//   });
+
+//   function updateCount() {
+//     itemCount.textContent = count;
+//   }
+// });
