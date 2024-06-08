0d274cac34a92ce22dd97d196c389314da90acc0
Morin Sultan <sultanmorin2001@gmail.com>
2024-06-08 23:03:20 +0100

    update

--- main.js
+++ main.js
@@ -1,3 +1,99 @@
+//Decelaring items
+const cart = document.querySelector(".cart-icon");
+const cartContain = document.querySelector(".cart-item");
+const menuOverlay = document.querySelector(".menu-overlay");
+const menuBtn = document.querySelector(".menu");
+const closeBtn = document.querySelector(".close-btn");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const productImg = document.querySelector(".main-image");
+const decreaseBtn = document.querySelector(".decrease");
+const increaseBtn = document.querySelector(".increase");
+const itemNumber = document.querySelector(".count");
+const cartNumber = document.querySelector(".cart-number");
+const nosOfItem = document.querySelector('.nos-of-item')
+const total = document.querySelector('.total')
+const cartDetails = document.querySelector(".cart-details");
+const cartMsg = document.querySelector(".cart-msg");
+let count = 1;
+let number = 0;
+console.log(productImg);
+
+cart.addEventListener("click", function () {
+  cartContain.classList.toggle("hidden");
+});
+
+menuBtn.addEventListener("click", function () {
+  menuOverlay.classList.add("show");
+});
+
+closeBtn.addEventListener("click", function () {
+  menuOverlay.classList.remove("show");
+});
+
+nextBtn.addEventListener("click", function () {
+  if (count == 4) {
+    count = 1;
+  } else {
+    count++;
+  }
+
+  productImg.setAttribute("src", `./images/image-product-${count}.jpg`);
+});
+
+prevBtn.addEventListener("click", function () {
+  if (count == 1) {
+    count = 4;
+  } else {
+    count--;
+  }
+
+  productImg.setAttribute("src", `./images/image-product-${count}.jpg`);
+});
+
+// Number of Item Added to cart
+
+increaseBtn.addEventListener("click", function () {
+  number++;
+  itemNumber.textContent = number;
+  if (number > 0) {
+    cartNumber.classList.remove("hidden");
+    cartNumber.textContent = number;
+    nosOfItem.textContent = number
+    let price = 125
+    price *= number;
+    total.textContent = ` $${price}`;
+    cartMsg.classList.add('hidden')
+    cartDetails.classList.remove('hidden')
+  } else {
+     cartMsg.classList.remove("hidden");
+     cartDetails.classList.add("hidden");
+  }
+  console.log(number);
+});
+
+decreaseBtn.addEventListener("click", function () {
+  if (number > 0) {
+    number--;
+    itemNumber.textContent = number;
+    cartNumber.textContent = number;
+    nosOfItem.textContent = number;
+    let price = 125
+    console.log(number, price);
+    price *= number;
+    total.textContent = ` $${price}`
+  } 
+  if (number == 0) {
+    cartNumber.classList.add("hidden");
+    cartMsg.classList.remove("hidden");
+    cartDetails.classList.add("hidden");
+  } 
+  console.log(number);
+});
+
+
+
+
 const mainImg = document.querySelectorAll(".product-image img");
 const thumblineImg = document.querySelectorAll(
   ".product-thumbnails .img-container"
@@ -41,46 +137,3 @@ console.log(mainImg);
 // });
 
 // Cart
-const cart = document.querySelector(".cart-icon");
-const cartContain = document.querySelector(".cart-item");
-const menuOverlay = document.querySelector(".menu-overlay");
-const menuBtn = document.querySelector(".menu");
-const closeBtn = document.querySelector(".close-btn");
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
-const productImg = document.querySelector(".main-image");
-let count = 1;
-console.log(productImg);
-
-cart.addEventListener("click", function () {
-  cartContain.classList.toggle("hidden");
-});
-
-menuBtn.addEventListener("click", function () {
-  menuOverlay.classList.add("show");
-});
-
-closeBtn.addEventListener("click", function () {
-  menuOverlay.classList.remove("show");
-});
-
-nextBtn.addEventListener("click", function () {
-  if (count == 4) {
-    count = 1;
-  } else {
-    count++;
-  }
-
-  productImg.setAttribute("src", `./images/image-product-${count}.jpg`);
-});
-
-prevBtn.addEventListener("click", function () {
-  if (count == 1) {
-    count = 4;
-  } else {
-    count--;
-  }
-
-  productImg.setAttribute("src", `./images/image-product-${count}.jpg`);
-});
-
